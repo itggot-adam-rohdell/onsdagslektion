c0a6f33be10c0ab0386a45cabf49388848e4ae9c
Adam Rohdell <adam.rohdell@itggot.se>
2017-02-07 00:57:36 +0100

    third

---
+++ HTTPTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace HTTPTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetDataAsync();
+            Console.ReadKey();
+        }
+
+        static async void GetDataAsync()
+        {
+            string result;
+            using (var client = new HttpClient())
+            {
+                result = await client.GetStringAsync("http://ur.se");
+            }
+            
+        }
+    }
+}
